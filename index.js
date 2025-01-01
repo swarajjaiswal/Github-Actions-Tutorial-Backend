f4b6207c914a55cc46261b1ca9fc97bd2e2af75b
swarajjaiswal <swaraj93310@gmail.com>
2025-01-01 20:56:13 +0530

    about added

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello World i am swaraj");
+});
+
+app.get("/about", (req, res) => {
+  res.send("I am Swaraj");
 });
 
 app.listen(3000, () => {
