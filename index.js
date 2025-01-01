2026264f5fc0e6eeb982461d518f96749d943710
swarajjaiswal <swaraj93310@gmail.com>
2025-01-01 23:22:43 +0530

    added contact

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get("/about", (req, res) => {
   res.send("I am Swaraj of nit dgp");
 });
 
+app.get("/contact", (req, res) => {
+  res.send("Contact me at");
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
